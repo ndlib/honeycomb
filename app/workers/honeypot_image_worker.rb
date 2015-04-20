7b63cddc934337c061a8c8f4cd66d3d3be1e90d6
Jaron Kennel <jaronkk@gmail.com>
2015-04-20 11:00:11 -0400

    Flailing at setting up the retry queue

--- app/workers/honeypot_image_worker.rb
+++ app/workers/honeypot_image_worker.rb
@@ -6,5 +6,19 @@ class HoneypotImageWorker < ActiveJob::QueueAdapters::SneakersAdapter::JobWrappe
     workers: 1,
     threads: 1,
     timeout_job_after: 60,
-    heartbeat_interval: 2
+    heartbeat_interval: 2,
+    prefetch: 1,
+    ack: true,
+    :'x-dead-letter-exchange' => 'honeypot_images-retry',
+    routing_key: ['honeypot_images']
+
+  def work(*args)
+    logger.info("HoneypotImageWorker rejecting args: #{args.inspect}")
+    begin
+      super(*args)
+    rescue Exception => e
+      Airbrake.notify(e, parameters: {args: args})
+      reject!
+    end
+  end
 end
--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,6 +1,13 @@
+require 'sneakers/handlers/maxretry'
+
 Sneakers.configure({
+  handler: Sneakers::Handlers::Maxretry,
   amqp: Rails.application.secrets.sneakers["amqp"],
   vhost: Rails.application.secrets.sneakers["vhost"],
   workers: 1,
   heartbeat: 5,
+  exchange: 'honeycomb',
+  exchange_type: 'topic',
+  routing_key: ['honeypot_images'],
+  durable: true,
 })
