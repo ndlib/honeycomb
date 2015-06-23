0a317dc99d8c9895b723796fa098e79aa429005e
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-06-23 12:51:00 -0400

    updates to add more of the fields in.
    Conflicts:
    	app/assets/javascripts/application.js
    	app/assets/javascripts/components.js
    	app/assets/javascripts/components/OpenSeadragonViewer.jsx
    	app/assets/javascripts/components/ReactDropzone.jsx
    	app/assets/javascripts/components/Thumbnail.jsx
    	app/assets/javascripts/components/showcase_editor/ShowcaseEditor.jsx
    	package.json

--- app/assets/javascripts/components/forms/DateField.js.jsx
+++ app/assets/javascripts/components/forms/DateField.js.jsx
@@ -21,6 +21,16 @@ var DateField = React.createClass({
     };
   },
 
+  getInitialState: function () {
+    return {
+      chooseDisplay: false,
+      year: null,
+      month: null,
+      day: null,
+      customDateDisplay: null
+    };
+  },
+
   requiredClass: function() {
     var css = 'form-control';
     if (this.props.required) {
@@ -33,9 +43,20 @@ var DateField = React.createClass({
   handleChange: function(event) {
     var year = this.refs.year.getDOMNode().value;
     var month = this.refs.month.getDOMNode().value;
+    var day = this.refs.day.getDOMNode().value;
+    var customDateDisplay = this.refs.customDateDisplay.getDOMNode().value;
 
-    this.props.handleFieldChange(this.props.name, year + "/" + month);
-    console.log(year + "/" + month)
+    this.setState({
+      year: year,
+      month: month,
+      day: day,
+      customDateDisplay: customDateDisplay
+    });
+    //this.props.handleFieldChange(this.props.name, year + "/" + month);
+  },
+
+  clickDateDisplayed: function() {
+    this.setState( { chooseDisplay: !this.state.chooseDisplay } );
   },
 
   formName: function() {
@@ -46,30 +67,33 @@ var DateField = React.createClass({
     return this.props.objectType + "_" + this.props.name;
   },
 
+  customDisplayField: function() {
+    if (this.state.chooseDisplay) {
+      return (
+        <div className="row">
+          <input  onChange={this.handleChange} type="text" ref="customDateDisplay" className="form-control" name="specifieddate" placeholder="Example: &quot;1788&quot; or &quot;circa. 1950&quot;" value={this.state.customDateDisplay} />
+        </div>
+      );
+    }
+    return "";
+  },
+
   render: function () {
     return (
         <FormRow id={this.formId()} type="string" required={this.props.required} title={this.props.title} help={this.props.help} errorMsg={this.props.errorMsg} >
           <div className="row">
-            <div className="col-xs-2">
-              <input type="text" name={this.formName()} value={this.props.value} className={this.requiredClass()} id={this.formId()} onChange={this.handleChange} placeholder="YYYY" ref="year" />
-            </div>
-            <div className="col-xs-2">
-              <input onChange={this.handleChange} type="text" name="text-2" placeholder="MM" className="form-control" ref="month" />
-            </div>
-            <div className="col-xs-2">
-              <input type="text" name="text-3" placeholder="DD" className="form-control" ref="day" />
-            </div>
+            <input type="number" pattern="\d*" max="2100" min="0" step="1" name={this.formName()} className={this.requiredClass()} id={this.formId()} onChange={this.handleChange} placeholder="YYYY" ref="year" style={{display: 'inline', width: '6em' }} value={this.state.year} />
+
+            <input onChange={this.handleChange} type="number" max="12" min="1" step="1"  name="text-2" placeholder="MM" className="form-control" style={{display: 'inline', width: '6em' }} ref="month" value={this.state.month} />
+
+            <input  onChange={this.handleChange} type="number" max="31" min="1" step="1" name="text-3" placeholder="DD" className="form-control" ref="day" style={{display: 'inline', width: '6em' }} value={this.state.day} />
           </div>
           <div className="row">
-            <div className="col-xs-12">
-              <input type="checkbox" name="specifydate" /> Let me choose how this date is displayed
-            </div>
+            <label className="help-block">
+              <input onClick={this.clickDateDisplayed} type="checkbox" name="specifydate" /> Let me choose how this date is displayed
+            </label>
           </div>
-          <div className="row">
-            <div className="col-xs-12">
-              <input type="text" className="form-control" name="specifieddate" placeholder="Example: &quot;1788&quot; or &quot;circa. 1950&quot;" />
-            </div>
-          </div>
+          {this.customDisplayField()}
         </FormRow>
       );
   }
