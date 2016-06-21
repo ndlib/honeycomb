c54cde75360e912563606ecb751756722ff60b37
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-06-21 14:28:01 -0400

    change the publish link

--- app/assets/javascripts/components/CollectionPreviewPublishLink.jsx
+++ app/assets/javascripts/components/CollectionPreviewPublishLink.jsx
@@ -4,53 +4,78 @@ var CollectionStore = require('stores/Collection');
 var CollectionPreviewPublishLink = React.createClass({
   propTypes: {
     previewLinkURL: React.PropTypes.string.isRequired,
-    previewLinkLabel: React.PropTypes.string.isRequired,
-    liveLinkLabel: React.PropTypes.string.isRequired,
-  },
-
-  componentWillMount: function() {
-    CollectionStore.on("CollectionStoreChanged", this.collectionStoreChanged);
   },
 
   getInitialState: function() {
     return {
-      published: CollectionStore.published,
-      preview: CollectionStore.preview,
+      copyMessage: null,
     };
   },
 
-  collectionStoreChanged: function() {
-    this.setState({
-      preview: CollectionStore.preview,
-      published: CollectionStore.published
-    });
-  },
-
-  linkLabel: function () {
-    if (this.state.published) {
-      return this.props.liveLinkLabel;
-    }
-    return this.props.previewLinkLabel;
-  },
-
-  siteLink: function() {
-    if(this.state.published || this.state.preview) {
-      return (
-        <a href={this.props.previewLinkURL} target="_blank">
-          <i className="glyphicon mdi-av-web"></i>
-          <span> {this.linkLabel()}</span>
-        </a>)
-    } else {
-      return "";
+  copyToClipBoard() {
+    this.refs.copyUrl.select();
+    try {
+        document.execCommand('copy');
+        this.setState({ copyMessage: true});
+      }
+    catch (err) {
+      this.setState({ copyMessage: false});
     }
   },
 
-  render: function () {
+
+  render() {
+
+    var copyMessage = '';
+    if(this.state.copyMessage !== null) {
+      if(this.state.copyMessage) {
+        copyMessage = (<div><i>The URL has been copied to your clipboard.</i></div>);
+      }
+      else {
+        copyMessage = (<div><i>Please press Ctrl/Cmd+C to copy.</i></div>);
+      }
+    }
+
     return (
       <div>
-        {this.siteLink()}
+        <h4>Copy Site URL</h4>
+        <div>
+          <input
+            ref={ 'copyUrl' }
+            type="text"
+            readOnly={ true }
+            name="copyUrl"
+            value={ this.props.previewLinkURL }
+            onClick= { this.copyToClipBoard }
+            style={{
+              maxWidth: '60%',
+              minWidth: '130px',
+              verticalAlign: 'top',
+            }}
+          /> <i
+            className="material-icons"
+            onClick={ this.copyToClipBoard }
+            style={{
+              backgroundColor: '#224048',
+              color: 'white',
+              cursor: 'pointer',
+              fontSize: '16px',
+              height: '26px',
+              padding: '5px',
+              width: '26px',
+            }}
+          >content_copy</i>
+        </div>
+        <br/>
+        { copyMessage }
+        <br />
+          <a href={this.props.previewLinkURL} target="_blank">
+            <i className="glyphicon mdi-av-web"></i>
+            <span> Preview Site</span>
+          </a>
       </div>
-    )
+    );
   }
+
 });
 module.exports = CollectionPreviewPublishLink;
