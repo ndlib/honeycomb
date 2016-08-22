eef3e29c26e865a9a946866459e727c830daf40b
Justin Gondron <jgondron@nd.edu>
2016-08-22 10:49:21 -0400

    DEC-1174: - Added mocks for s3 stuff to specs

--- spec/services/buzz_media_spec.rb
+++ spec/services/buzz_media_spec.rb
@@ -6,12 +6,8 @@ RSpec.describe BuzzMedia do
   let(:media_server_json) { JSON.parse(File.read(File.join(Rails.root, "spec/fixtures/buzz_response.json"))) }
   let(:collection) { instance_double(Collection, id: 100) }
 
-  let(:s3) { double(bucket: bucket) }
-  let(:bucket) { double(object: bucket_object) }
-  let(:bucket_object) { double(presigned_url: "presigned url", public_url: "public url") }
-
   before(:each) do
-    allow_any_instance_of(AllocateS3Url).to receive(:s3).and_return(s3)
+    allow(AllocateS3Url).to receive(:public_url).and_return("public url")
   end
 
   let(:video) do
@@ -70,7 +66,7 @@ RSpec.describe BuzzMedia do
       it "sends a create request to the media server" do
         expect(media_server_connection).to receive(:post).with(
           "/v1/media_files",
-          media_file: { file_path: bucket_object.public_url, media_type: "video" }
+          media_file: { file_path: "public url", media_type: "video" }
         ).and_return(media_response)
 
         subject.create
