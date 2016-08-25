0fd2e6341abf9f254e11ac0cadc680b16ccc15a8
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-08-25 14:55:56 -0400

    migrate.

--- db/migrate/20160825183434_change_page_image_to_media.rb
+++ db/migrate/20160825183434_change_page_image_to_media.rb
@@ -1,7 +1,13 @@
 class ChangePageImageToMedia < ActiveRecord::Migration
-  def change
+  def up
     remove_foreign_key :pages, :images
     rename_column :pages, :image_id, :media_id
     add_foreign_key :pages, :media, column: :media_id
+  end
+
+  def down
+    remove_foreign_key :pages, column: :media_id
+    rename_column :pages, :media_id, :image_id
+    add_foreign_key :pages, :images
   end
 end
