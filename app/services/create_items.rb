c8976c1f520af95bbac595bae8b9528b6a7a5cc2
Justin Gondron <jgondron@nd.edu>
2017-01-24 09:56:37 -0500

    DEC-1344: -Adding code I'm using to profile the imports. This one is based on v3.6.0 which does not have the parent/child associations

--- app/services/create_items.rb
+++ app/services/create_items.rb
@@ -26,12 +26,27 @@ class CreateItems
   #   total_count    - Total number of items processed
   def create_or_update!(collection:, find_by:, items_hash:, counts:, errors:)
     items_to_index = []
+
+rewrite_block_bm = Benchmark::Tms.new
+find_or_create_bm = Benchmark::Tms.new
+save_item_bm = Benchmark::Tms.new
+other_bm = Benchmark::Tms.new
+
     ActiveRecord::Base.transaction do
       items_hash.each.with_index do |item_props, index|
         rewrite_errors = []
+rewrite_block_bm.add!() do
         item_props = yield(item_props, rewrite_errors).symbolize_keys if block_given?
+end
+item_creator = nil
+find_or_create_bm.add!() do
         item_creator = FindOrCreateItem.call(props: { collection_id: collection.id, **item_props }, find_by: find_by)
+end
+saved = nil
+save_item_bm.add!() do
         saved = rewrite_errors.present? ? false : item_creator.save(index: false)
+end
+other_bm.add!() do
         add_to_errors(
           errors: errors,
           index: index,
@@ -40,9 +55,23 @@ class CreateItems
         )
         items_to_index << item_creator.item if saved && (item_creator.changed? || item_creator.new_record?)
         update_counts(save_successful: saved, item: item_creator, counts: counts)
+end
       end
     end
+
+index_bm = Benchmark.measure(label: "Index") do
     Index::Collection.index!(collection: collection, items: items_to_index)
+end
+
+total_bm = rewrite_block_bm + find_or_create_bm + save_item_bm + other_bm + index_bm
+puts "Count: #{items_hash.count}"
+puts                         ("               user     system      total        real")
+puts rewrite_block_bm.format ("Rewrite block  %U %y %t %r")
+puts find_or_create_bm.format("Find or create %U %y %t %r")
+puts save_item_bm.format     ("Save item      %U %y %t %r")
+puts other_bm.format         ("Other          %U %y %t %r")
+puts index_bm.format         ("Index          %U %y %t %r")
+puts total_bm.format         ("Total          %U %y %t %r")
   end
 
   private
