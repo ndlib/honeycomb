995f6de3d2934acc7057618caaf3107a659ca059
ialford <ialford@nd.edu>
2020-09-18 10:50:21 -0400

    update aws script

--- docker/solr_aws_precreate.sh
+++ docker/solr_aws_precreate.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 # Doing the tasks that are needed to use persistent EFS in Fargate
 
-echo "WOOP WOOP"
-
 if [ -d "/mnt/solr" ]
 then
     cp -R /opt/solr/server/solr/ /mnt/solr
-    ls /mnt/solr/solr/
-else
-    ls /mnt/solr/
 fi
