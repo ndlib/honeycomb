40c5d15fb738928016a33b23fc4dce47caa29b6f
Justin Gondron <jgondron@nd.edu>
2021-05-13 15:22:46 -0400

    Fix public images
    This fixes an issue with uploading images when deployed to ECS. Rails
    expects public/system to exist, but is not kept in git, so image uploads
    were failing to get moved by paperclip to the correct dir. Changed the
    link of /project_root/public/system/images/honeycomb -> /mnt/honeycomb
    to instead be a link of /project_root/public/system -> /mnt/system. The
    issue was likely obfuscated locally because I had files in public/system
    from a local execution of rails, so the docker build was copying these
    in. Changed the dockerignore to ignore this directory so that the
    behavior more closely match what the docker context will look like in a
    fresh checkout.
    
    Additionally moved away from running rails as root and added a rails user
    (uid 999) that will run the processes in the container.

--- docker/rails_entry.sh
+++ docker/rails_entry.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
 # All the things that will execute when starting the rails service
-ln -s /mnt/honeycomb /project_root/public/system/images
+set -e
+
+# In a mutable host scenario, Capistrano would handle symlinking ./public/system to a persistent
+# directory on the host. We'll have to recreate this pattern by providing a mount target (/mnt/system) 
+# that we'll mount a persistent volume to later and symlink ./public/system to that mount point. 
+# Note: We can create the mount point as part of the build, but we'll have to symlink it in 
+# rails_entry.sh since Docker does not allow symlinking outside of the context
+ln -s /mnt/system /project_root/public/
 
 ### Wait for dependencies
 if ! docker/wait-for-it.sh -t 120 ${DB_HOSTNAME}:5432; then exit 1; fi
